60d6b2ef9c1d8d15f0e3c76b019944bafa43da58
sefaapinar <sefanur.pinar@gmail.com>
2022-02-08 11:18:41 +0300

    Data-Types-Variables

---
+++ variables-data-types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace variables_data_types{
+    
+
+    class Program{
+        static void Main(String[] args){
+            
+            int deger;
+            string degisken = null;                     //Değişken isimleri büyük küçük harf uyumludur.
+            
+
+
+
+            Console.WriteLine(deger);
+        }
+    }
+}
