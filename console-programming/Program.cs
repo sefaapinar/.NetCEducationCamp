a995832a246f5c0cf250d292abe540e927f38246
sefaapinar <sefanur.pinar@gmail.com>
2022-02-08 11:08:29 +0300

    First Lesson - Basic

---
+++ console-programming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace console_programming
+{
+    class Program
+    {
+        public static void Main(string[] args){
+            
+            Console.WriteLine("Adinizi Giriniz: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyadinizi Giriniz");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+
+        }
+    }
+}
